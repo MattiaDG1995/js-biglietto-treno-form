2c56aa00269516daaa21e04cb3927d822721eac7
MattiaDG1995 <mattiadigennaro95@gmail.com>
2022-10-20 17:03:38 +0200

    ticket

--- main.js
+++ main.js
@@ -1,25 +1,42 @@
 function start(){
+    let valuenome = document.getElementById('nome').value;
+    document.getElementById('nomepass').innerHTML = `Nome Passeggero ${valuenome}`;
     let valuekilometri = document.getElementById('kilometri').value;
-    let valueetà = document.getElementById('età').value;
+    let etaPasseggero = document.getElementById('etaPasseggero').value;
+    let minorenne = document.getElementById('minorenne').value;
+    let over65 = document.getElementById('over65').value;
     let prezzoKilometro = 0.21;
     let prezzoBiglietto = valuekilometri * prezzoKilometro;
-    let sconto ;
+    let sconto = document.getElementById('sconto').value;
+    // let standard = document.getElementById('standard').value;
     
     
-    if (valueetà < 18){
+    if (etaPasseggero == minorenne){
     
         sconto = (prezzoBiglietto  / 100) * 20;
-    
-    } else if (valueetà >= 65){
+        document.getElementById('sconto').innerHTML = `offerta ${sconto}%`;
+
+    } else if (etaPasseggero == over65){
     
         sconto = (prezzoBiglietto  / 100) * 40;
-    
+        document.getElementById('sconto').innerHTML = `offerta ${sconto}%`;
+
     } else {
     
         sconto = 0;
+        document.getElementById('sconto').innerHTML = `offerta ${sconto}%`;
+        
     }
     
     let prezzoTotale = parseFloat(prezzoBiglietto - sconto).toFixed(2);
-    
-    document.getElementById('prezzotot').innerHTML = `il prezzo totale è di ${prezzoTotale} &euro;`;
+    document.getElementById('prezzotot').innerHTML = `Costo biglietto ${prezzoTotale} &euro;`;
+
+    let numCarrozza = document.getElementById('numCarrozza').value;
+    document.getElementById('numCarrozza').innerHTML = Math.floor(Math.random() * 10) + 1 ;
+
+
+    let codecp = document.getElementById('codecp').value;
+    document.getElementById('codecp').innerHTML = Math.floor(Math.random() * 100000) + 1;
+
+
     }
